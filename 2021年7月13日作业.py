f684035eb4e40c1d2d712250075f242ebc596bab
zhangtengccie <zhangtengccie@gmail.com>
2021-07-18 19:13:57 +0800

    python_standard

---
+++ 2021年7月13日作业.py
@@ -0,0 +1,86 @@
+from kamene.all import *
+from kamene.layers.inet import ICMP, IP
+
+
+class Qping():
+    def __init__(self, ipaddr):
+        self.ipaddr = ipaddr
+        self.srcip = None
+        self.length = 100
+
+    def make_pkt(self):
+        if self.srcip:
+            self.pkt = IP(dst=self.ipaddr, src=self.srcip) / ICMP() / b'v' * self.length
+        else:
+            self.pkt = IP(dst=self.ipaddr) / ICMP() / b'v' * self.length
+
+    def one(self):
+        self.make_pkt()
+        ping_result = sr1(self.pkt, timeout=2, verbose=False)
+        # print(self.ping_pkt.show())
+        if ping_result:
+            print(f'{self.ipaddr} 可达！')
+        else:
+            print(f'{self.ipaddr} 不可达！')
+
+    def ping_five(self):
+        self.make_pkt()
+        for i in range(5):
+            ret = sr1(self.pkt, timeout=2, verbose=False)
+            if ret:
+                print('!', end='', flush=True)
+            else:
+                print('.', end='', flush=True)
+        print()
+
+    def __str__(self):
+        if self.srcip is None:
+            return '<%5s=>dest_ip: %5s,size: %s>' % (self.__class__.__name__, self.ipaddr, self.length)
+        else:
+            return '<%5s=>dest_ip: %5s,src_ip:%5s,size: %s>' % (
+                self.__class__.__name__, self.ipaddr, self.srcip, self.length)
+
+
+class Newping(Qping):
+    def ping_five(self):
+        self.make_pkt()
+        for i in range(5):
+            ret = sr1(self.pkt, timeout=1, verbose=False)
+            # print(self.ping_pkt.show())
+            if ret:
+                print('!', end='', flush=True)
+            else:
+                print('+', end='', flush=True)
+        print()
+
+
+
+
+if __name__ == '__main__':
+    ping = Qping('1.1.1.200')
+    total_len = 70
+
+
+    def pring_new(word, s='-'):
+        print('{0}{1}{2}'.format(s * int((70 - len(word)) / 2), word, s * int((70 - len(word)) / 2)))
+
+
+    pring_new('print class')
+    print(ping)
+    pring_new('ping one for sure reachable')
+    ping.one()
+    pring_new('ping five')
+    ping.ping_five()
+    pring_new('set payload lenth')
+    ping.length = 200
+    print(ping)
+    ping.ping_five()
+    pring_new('set ping src ip address')
+    ping.srcip = '2.2.2.20'
+    print(ping)
+    ping.ping_five()
+    pring_new('new class NewPing', '=')
+    newping = Newping('1.1.1.200')
+    newping.length = 300
+    print(newping)
+    newping.ping_five()
